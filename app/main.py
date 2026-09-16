a8a379982800a9e38849821f207fd91d7c6b97d3
VrajPatel105 <vrajp1052005@gmail.com>
2026-09-16 19:16:14 -0400

    Add(main) : /health endpoint

---
+++ app/main.py
@@ -0,0 +1,29 @@
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from fastapi import HTTPException
+import time
+
+# module level flag
+is_ready = False
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    
+    global is_ready
+    time.sleep(15)
+    is_ready = True
+    
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
+
+@app.get("/")
+async def root():
+    return {"message" : "Hey There, Welcome to Paged Inference Serving Engine"}
+
+@app.get("/health")
+async def health():
+    if not is_ready:
+        raise HTTPException(status_code=503, detail="not ready")
+    return {"status": "ready"}
