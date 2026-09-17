798732d8e1b4206f16c6e065d8bcc80414c5340c
VrajPatel105 <vrajp1052005@gmail.com>
2026-09-16 22:13:00 -0400

    Adding the async context manager for fastapi with warmup and model/tokenizer loading along with initializing the objects

--- app/main.py
+++ app/main.py
@@ -1,20 +1,63 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi import HTTPException
-import time
 from app.schemas import UserInput
+import queue
+from core.model_runner import Engine
+import pickle
+import threading
+from transformer.load_checkpoint import load_trained_weights
+import time
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-# module level flag
+CHECKPOINT_PATH = os.environ.get("CHECKPOINT_PATH")
+TOKENIZER_PATH = os.environ.get("TOKENIZER_PATH")
+
+engine = None
+engine_thread = None
 is_ready = False
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     
-    global is_ready
-    time.sleep(15)
-    is_ready = True
+    global engine, engine_thread, is_ready
+
+    global_start_time = time.time()
+
+    model = load_trained_weights(CHECKPOINT_PATH)
+
+    with open(TOKENIZER_PATH, 'rb') as f:
+        tok = pickle.load(f)
+
+    engine = Engine(model, tok)
+    engine_thread = threading.Thread(target=engine.run, daemon=True)
+    engine_thread.start()
+
+    #warmup
+    warmup_sentence =  "This is a warmup text to turn on the engineeeeeeeeee (vroommmmmmmm)"
+    warmup_seq_id = engine.submit(tok.encode_sentence(warmup_sentence,  add_sos=True, add_eos=False)) # submitting the list of ints (encoded from tokenizer)
+    start_time = time.time()
+    warmup_ok = 0 # 0 : success, 1 : failure
+    while warmup_seq_id not in engine.results:
+        elapsed_time = time.time() - start_time
+        if elapsed_time > 30:
+            print("broke the loop since it took more than 30 seconds for warmup")
+            warmup_ok = 1
+            break
+        time.sleep(0.1)
+    if warmup_ok == 0:
+        print("warmup done \n")
+        print("total time taken: ", time.time()-start_time)
+        print("global start time", time.time()-global_start_time)
+        is_ready = True
     
     yield
+
+    # for stopping, get the stop method in engine
+    engine.stop()
+    engine_thread.join()
 
 app = FastAPI(lifespan=lifespan)
 
